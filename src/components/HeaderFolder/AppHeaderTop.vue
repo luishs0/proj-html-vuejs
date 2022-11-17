b3ccfedba268457eb9920f63639c609c5cb01f05
luishs0 <luishs.doc@gmail.com>
2022-11-17 17:51:01 +0100

    header-top

--- src/components/HeaderFolder/AppHeaderTop.vue
+++ src/components/HeaderFolder/AppHeaderTop.vue
@@ -7,17 +7,44 @@ export default {
         return {
             store
         }
-    }
+    },
 }
 </script>
 
 
 <template>
+    <!-- Message up -->
     <div class="msg d-flex flex-wrap justify-content-center align-items-center">
         <span class="me-4 msg-s">Starts TOMORROW! Our biggest event of the year...</span>
         <span class="time me-4"> <i class="fa-regular fa-clock"></i> 00 : 00 : 00 : 00</span>
         <button class="btn btn-primary">Get Ticket</button>
     </div>
+    <!-- /Message up -->
+
+    <!-- Navbar -->
+    <div class="container">
+        <div class="ms_navbar d-flex justify-content-between align-items-center">
+
+            <div class="logo">
+                <a href=""><img src="../../assets/img/dark-logo.png" alt=""></a>
+            </div>
+
+            <div class="links">
+                <a v-for="link in this.store.headerLinks" href=""> {{ link }} <i class="fa-solid fa-chevron-down"></i>
+                </a>
+            </div>
+
+            <div class="social">
+                <a href=""><i class="fa-brands fa-twitter"></i></a>
+                <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+                <a href=""><i class="fa-brands fa-instagram"></i></a>
+                <a href=""><i class="fa-brands fa-linkedin"></i></a>
+
+            </div>
+
+        </div>
+    </div>
+    <!-- /Navbar -->
 </template>
 
 
@@ -44,5 +71,46 @@ export default {
     }
 
 
+}
+
+.ms_navbar {
+    padding: 1.3rem 0;
+
+    .logo {
+
+        img {
+            width: 7rem;
+        }
+    }
+
+    .links {
+
+
+        a {
+            text-decoration: none;
+            color: $primary-gray;
+            font-size: .8rem;
+
+            i {
+                font-size: .5rem;
+                vertical-align: middle;
+            }
+        }
+
+        & a:not(:last-child) {
+            margin-right: 1.8rem;
+        }
+    }
+
+    .social {
+        a {
+            text-decoration: none;
+            color: $secondary-gray;
+        }
+
+        & a:not(:last-child) {
+            margin-right: 1rem;
+        }
+    }
 }
 </style>
