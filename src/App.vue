fdb9a30de6f552cef86a76fb0571c8d3b247934d
luishs0 <luishs.doc@gmail.com>
2022-11-17 13:07:30 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import { store } from "./store";
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  components: {
+    // Files .vue
+  },
+  created() {
+    // Funzioni inmediate
+  },
+  methods: {
+    // Metods
+  }
+}
+
+</script>
+
+
+<template>
+  <h1>Hello</h1>
+  <p>As a coach</p>
+</template>
+
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+@use "./styles/partials/variables" as *;
+</style>
